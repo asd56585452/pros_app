7f0b3fd98641015a252faca842c567029f92e1c6
asd56585452 <a565854525658545256585452@gmail.com>
2026-03-12 11:48:47 +0800

    new urdf publish

--- slam_unity.sh
+++ slam_unity.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 source "./utils.sh"
-main "./docker/compose/docker-compose_rplidar_unity.yml" "./docker/compose/docker-compose_slam_unity.yml"
+main "./docker/compose/docker-compose_robot_unity.yml" "./docker/compose/docker-compose_slam_unity.yml"
